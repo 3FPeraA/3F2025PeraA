cd6304ebe2190c4d04c1f9f80127763592582c6f
unknown <pera.alessio.2023@itisgalileiroma.it>
2025-11-06 09:36:54 +0100

    Esercizi LAB 06/11/2025

--- 06-11-2025_lab/bubblesort.cpp
+++ 06-11-2025_lab/bubblesort.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-    cout<<"HELLO WORLD!";
+    int numeri[5] = {8,6,12,1,5};
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5 - 1; j++) {
+            if (numeri[j] > numeri[j + 1]) {
+                int swap=numeri[j];
+                numeri[j]=numeri[j + 1];
+                numeri[j + 1]=swap;
+            }
+        }
+    }
+    for (int i = 0; i < 5; i++) {
+        cout << numeri[i] <<endl;
+    }
 }
