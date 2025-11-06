0bc2af199cdef901dd39c767db97e38c9b8bcced
unknown <pera.alessio.2023@itisgalileiroma.it>
2025-11-06 09:49:13 +0100

    Esercizio LAB 06/11/2025

--- 06-11-2025_lab/bubblesort.cpp
+++ 06-11-2025_lab/bubblesort.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 int main() {
     int size;
-    //int size = 5;
     cout<<"Quanti numeri vuoi inserire: ";
     cin>>size;
     int numeri[size];
     for (int i=0; i<size; i++) {
-        cout<<"Inserisci valore: ";
+        cout<<"1Inserisci valore: ";
         cin>>numeri[i];
     }
-    //int numeri[size] = {8,6,12,1,5};
+    //BUBBLE SORT:
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size - 1; j++) {
             if (numeri[j] > numeri[j + 1]) {
